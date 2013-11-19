b6efcc86932136bd111a38cc3cf66ece1bb4e813
yuetingqian <yuetingqian@gmail.com>
2013-11-19 11:11:32 +0800

    bash set -e

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import md5
 import unittest
 class md5Test(unittest.TestCase):
     def test_md5(self):
-        self.assertEqual(mymd5(), md5.new('1234'))
+        self.assertEqual(mymd5(), md5.new('123'))
 
 if __name__ == '__main__':
     unittest.main()
--- test.sh
+++ test.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -e
 coverage run test.py
 coverage report -m
 coverage html
